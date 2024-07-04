fcb3605698519f8dd4f50c27c5ef8ccb0846a42c
ChenXy128 <e0968917@u.nus.edu>
2024-07-04 14:00:25 +0800

    able to recommend from the fake shop api

---
+++ pinKUNicorn/FakeShop.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const fetchProducts = async () => {
+  try {
+    const response = await axios.get('https://fakestoreapi.com/products');
+    console.log(response.data)
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    throw new Error('Failed to fetch products');
+  }
+};
+
+export default fetchProducts;
