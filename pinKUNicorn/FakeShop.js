7d1c199a802be2f1f2822d7de87e2859c2ebaee3
ChenXy128 <e0968917@u.nus.edu>
2024-07-04 15:49:26 +0800

    adjusted prompt

--- pinKUNicorn/FakeShop.js
+++ pinKUNicorn/FakeShop.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const fetchProducts = async () => {
   try {
     const response = await axios.get('https://fakestoreapi.com/products');
-    console.log(response.data)
+    //console.log(response.data)
     return response.data;
   } catch (error) {
     console.error('Error fetching products:', error);
